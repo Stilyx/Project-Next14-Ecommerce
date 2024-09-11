737da84e4d7f5ad251fcd4f004f5250deb07acb7
Steykis <steykis@gmail.com>
2024-09-11 14:59:48 -0300

    style: fix sections sizes

--- src/layouts/about/index.tsx
+++ src/layouts/about/index.tsx
@@ -2,7 +2,7 @@ import { SectionCard } from "@/components/sectionCard";
 
 export const About = () => {
   return (
-    <SectionCard className="mb-[12.5rem] flex w-full flex-row items-center justify-center gap-[7.813rem] tablet:mb-[6rem] tablet:flex-col-reverse tablet:items-center tablet:gap-[4.063rem] mobile:mb-[7.5rem]">
+    <SectionCard className="mb-[12.5rem] flex w-full flex-row items-center justify-center gap-[9.813rem] tablet:mb-[6rem] tablet:flex-col-reverse tablet:items-center tablet:gap-[4.063rem] mobile:mb-[7.5rem]">
       <section className="flex w-[27.813rem] flex-col gap-[2rem] tablet:text-center mobile:w-full">
         <h2 className="prose-headline-h2 mobile:prose-headline-h4">
           Bringing you the <span className="text-primary-200">best</span> audio
@@ -18,7 +18,7 @@ export const About = () => {
         </p>
       </section>
 
-      <div className="h-[36.75rem] w-[50%] bg-contain bg-right bg-no-repeat desktop:bg-best-gear-desktop tablet:h-[18.75rem] tablet:w-full tablet:bg-best-gear-tablet tablet:bg-contain tablet:bg-center mobile:bg-best-gear-mobile"></div>
+      <div className="h-[36.75rem] w-[540px] bg-contain bg-right bg-no-repeat desktop:bg-best-gear-desktop tablet:h-[18.75rem] tablet:w-full tablet:bg-best-gear-tablet tablet:bg-contain tablet:bg-center mobile:bg-best-gear-mobile"></div>
     </SectionCard>
   );
 };
