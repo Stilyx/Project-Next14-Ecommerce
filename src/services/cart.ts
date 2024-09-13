f4418b5a8d01bd74966d7e468c1275a2ece2f74b
daniel <dani.perfil.profissional@gmail.com>
2024-09-13 12:25:06 -0300

    feat: adding exports on services

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -4,58 +4,58 @@ import { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 
 export const getCart = async () => {
-	const response: AxiosResponse<ICart[]> = await instance.get("/cart");
-	return response.data;
+  const response: AxiosResponse<ICart[]> = await instance.get("/cart");
+  return response.data;
 };
 
 export const addItemToCart = async (data: ICart) => {
-	await instance
-		.post("/cart", data)
-		.then((response) => {
-			toast.success(`${data.shortName} foi adicionado ao carrinho`);
-			return response.data;
-		})
-		.catch((err) =>
-			toast.error(`Não foi posível adicionar o produto. Erro: ${err}`)
-		);
+  await instance
+    .post("/cart", data)
+    .then((response) => {
+      toast.success(`${data.shortName} foi adicionado ao carrinho`);
+      return response.data;
+    })
+    .catch((err) =>
+      toast.error(`Não foi posível adicionar o produto. Erro: ${err}`),
+    );
 };
 
 export const changeProductQuantity = async (
-	id: string,
-	quantity: { quantity: number }
+  id: string,
+  quantity: { quantity: number },
 ) => {
-	instance
-		.patch(`/cart/${id}`, quantity)
-		.then(() => toast.success("Atualizado com sucesso"))
-		.catch((err) =>
-			toast.error(`Não foi posível atualizar quantidade. Erro: ${err}`)
-		);
+  instance
+    .patch(`/cart/${id}`, quantity)
+    .then(() => toast.success("Atualizado com sucesso"))
+    .catch((err) =>
+      toast.error(`Não foi posível atualizar quantidade. Erro: ${err}`),
+    );
 };
 
 export const readProduct = async (cartList: ICart[]) => {
-	try {
-		cartList.forEach((item) => {
-			instance.patch(`/cart/${item.id}`, { new: false });
-		});
-	} catch (err) {}
+  try {
+    cartList.forEach((item) => {
+      instance.patch(`/cart/${item.id}`, { new: false });
+    });
+  } catch (err) {}
 };
 
 export const deleteAllCart = async (cartList: ICart[]) => {
-	try {
-		cartList.forEach((item) => {
-			instance.delete(`/cart/${item.id}`);
-		});
-		toast.success("Produtos deletados!");
-	} catch (err) {
-		toast.error(`Não foi posível deletar produtos. Erro: ${err}`);
-	}
+  try {
+    cartList.forEach((item) => {
+      instance.delete(`/cart/${item.id}`);
+    });
+    toast.success("Produtos deletados!");
+  } catch (err) {
+    toast.error(`Não foi posível deletar produtos. Erro: ${err}`);
+  }
 };
 
 export const deleteProductId = async (id: string) => {
-	try {
-		instance.delete(`/cart/${id}`);
-		toast.success("Produto deletado!");
-	} catch (err) {
-		toast.error(`Não foi posível deletar produto. Erro: ${err}`);
-	}
+  try {
+    instance.delete(`/cart/${id}`);
+    toast.success("Produto deletado!");
+  } catch (err) {
+    toast.error(`Não foi posível deletar produto. Erro: ${err}`);
+  }
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,10 @@
 export {
-	addItemToCart,
-	changeProductQuantity,
-	deleteAllCart,
-	deleteProductId,
-	getCart,
-	readProduct,
+  addItemToCart,
+  changeProductQuantity,
+  deleteAllCart,
+  deleteProductId,
+  getCart,
+  readProduct,
 } from "./cart";
 export { getCategory } from "./category";
+export { getProduct } from "./product";
